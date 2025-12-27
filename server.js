a0c9e0fad143e9e126351a1f9b4582904a697c80
Sijo Santhosh <sijosanthosh2@gmail.com>
2025-12-27 22:32:49 +0530

    Code updated

--- server.js
+++ server.js
@@ -4,25 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const productRoutes = require('./routes/products');
 
-const app = express();
-app.set("trust proxy", true); // IMPORTANT for Render
-
-// 🔁 Force HTTPS + www (SEO)
-app.use((req, res, next) => {
-  const host = req.headers.host;
-  const protocol = req.headers["x-forwarded-proto"] || req.protocol;
-
-  if (protocol !== "https") {
-    return res.redirect(301, `https://${host}${req.originalUrl}`);
-  }
-
-  if (!host.startsWith("www.")) {
-    return res.redirect(301, `https://www.${host}${req.originalUrl}`);
-  }
-
-  next();
-});
-
+const app = express();  
 app.use(cors());
 app.use(express.json());
 
@@ -37,3 +19,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/', (req, res) => res.send('Backend running!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
