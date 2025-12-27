24efb981779b2ee722cbe26db37b30854b5c0896
Sijo Santhosh <sijosanthosh2@gmail.com>
2025-12-27 22:44:37 +0530

    Cors updated

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 const productRoutes = require('./routes/products');
 
-const app = express();  
-app.use(cors());
+const app = express();
+
+app.use(cors({
+  origin: [
+    "https://www.santhoshfoam.com",
+    "https://santhoshfoam.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
@@ -19,5 +28,3 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/', (req, res) => res.send('Backend running!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
